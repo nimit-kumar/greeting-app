0465fd8a5ae55ae4ab44fff96e17a264a09c3db3
nimit-kumar <nimitkumar9760@gmail.com>
2025-06-19 15:32:43 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,1 +1,14 @@
-print("ram")
+import time
+
+b= time.strftime("%H", time.localtime())
+a=int(b)
+print("The time is",time.strftime("%I:%M:%S:%P",time.localtime()))
+if(a>5 and a<12):
+  print("Good morning")
+elif(a>=12 and a<17):
+  print("Good afternoon ")
+elif(a>=17 and a<21):
+  print("Good evening")
+else:
+  print("Good night")
+
